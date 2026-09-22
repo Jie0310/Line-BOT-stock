065fdeb9509500bb3c5036c21712fc8e85983a15
Jie0310 <yijhuang8931@gmail.com>
2026-09-22 17:31:52 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask, abort, request
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import ApiClient, Configuration, MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+
+app = Flask(__name__)
+
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+
+if channel_access_token is None or channel_secret is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN and LINE_CHANNEL_SECRET environment variables.')
+    SystemExit(1)
+
+handler = WebhookHandler(channel_secret)
+configuration = Configuration(access_token=channel_access_token)
+
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: ' + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        user_text = event.message.text
+
+        reply_text = f'你查詢的股票代號是：{user_text}\n(權證推薦功能建置中...) '
+
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply_text)],
+            )
+        )
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
